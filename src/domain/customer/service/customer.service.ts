c268a946e61af6de21d5f65276f02dabf8a25dc4
Everton Oliveira <evertonoliveira@Evertons-MBP.bbrouter>
2023-11-16 08:14:25 -0300

    fix: remove transaction

--- src/domain/customer/service/customer.service.ts
+++ src/domain/customer/service/customer.service.ts
@@ -1,25 +1,22 @@
+import { v4 as uuid } from 'uuid';
 import CustomerRepository from "../../../infrastructure/customer/repository/sequelize/customer.repository";
-import { TransactionInterface } from "../../@shared/domain/transaction.interface";
 import { Mediator } from "../../@shared/service/mediator";
 import Customer from "../entity/customer";
-import {v4 as uuid} from 'uuid'
 
 
 export class CustomerService{
 
     constructor(
         private customerRepo: CustomerRepository, 
-        private mediator: Mediator,
-        private transaction: TransactionInterface
+        private mediator: Mediator
         ){}
 
     async create(name: string){
         const customer = Customer.create(uuid(), name);
-        this.transaction.do(async (transaction) => {
-            this.customerRepo.setTransaction(transaction)
-            await this.customerRepo.create(customer);
-            await this.mediator.publish(customer); 
-        })
+        
+        await this.customerRepo.create(customer);
+        await this.mediator.publish(customer); 
+        
         return customer;
     }
 }
